178aebc2a90e642ed8084e551ac205964541defa
PikachuHEX <me.saurav2015@gmail.com>
2023-04-05 22:40:31 -0400

    Did it

--- pages/home.js
+++ pages/home.js
@@ -2,3 +2,28 @@ $(function(){
     $("#header").load("header.html"); 
     $("#footer").load("footer.html"); 
 });
+$(".like-btn").on("click", function () {
+    var postId = $(this).data("id");
+    var likeCount = $(this).siblings(".like-count");
+    var likeCountValue = parseInt(likeCount.text());
+    $(this).toggleClass("text-success");
+    $(this).children().toggleClass("fas");
+    if ($(this).hasClass("text-success")) {
+        likeCount.text(likeCountValue + 1);
+    } else {
+        likeCount.text(likeCountValue - 1);
+    }
+});
+
+$(".dislike-btn").on("click", function () {
+    var postId = $(this).data("id");
+    var dislikeCount = $(this).siblings(".dislike-count");
+    var dislikeCountValue = parseInt(dislikeCount.text());
+    $(this).toggleClass("text-danger");
+    $(this).children().toggleClass("fas");
+    if ($(this).hasClass("text-danger")) {
+        dislikeCount.text(dislikeCountValue + 1);
+    } else {
+        dislikeCount.text(dislikeCountValue - 1);
+    }
+});
