9c7617c4f5abce0ba3bf36df91055bc94b61a9fe
PikachuHEX <me.saurav2015@gmail.com>
2023-04-07 19:23:54 -0400

    Tweak 1.0

--- pages/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const footerContainer = document.getElementById("footer-container");
-  const footerUrl = "footer.html";
-  fetch(footerUrl)
-    .then(response => response.text())
-    .then(data => {
-      footerContainer.innerHTML = data;
-    });
-});
--- pages/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const headerContainer = document.getElementById("header-container");
-  const headerUrl = "header.html";
-  fetch(headerUrl)
-    .then(response => response.text())
-    .then(data => {
-      headerContainer.innerHTML = data;
-    });
-});
